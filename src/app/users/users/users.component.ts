8a9d62cf97a176ac0b20829e56b5bd972b40fbfe
phuongcse <trantuanphuong19941994@gmail.com>
2018-08-20 22:02:18 +0700

    property binding

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -8,45 +8,51 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended = true;
+  loaded = false;
+  enableAdd = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'MA'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Williams',
-        age: 36,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'MA'
-        }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 24,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'MA'
-        }
-      },
-    ];
-  }
-
-  addUser(user: User) {
-    this.users.push(user);
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+          },
+          image: 'http://lorempixel.com/600/600/people/3'
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 36,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'MA'
+          },
+          image: 'http://lorempixel.com/600/600/people/1'
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 24,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+          },
+          image: 'http://lorempixel.com/600/600/people/2'
+        },
+      ];
+      this.loaded = true;
+    }, 2000);
+    this.showExtended = true;
   }
 }
